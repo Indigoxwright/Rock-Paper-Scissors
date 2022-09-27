f21b8006517bcedce3954f39707cbb71f0401128
Indigo <9696.ewright@gmail.com>
2022-09-27 12:27:14 -0700

    Fix random word

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
-function getComputerChoice ()
-  var array = ["Rock", "Paper", "Scissors"]
-  var randInt = randomGenerator(0, array.length - 1);
-  var item = array[randInt];
+var words = ['Rock', 'Paper', 'Scissors',];
+function getComputerChoice(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+for(var x=0; x<20; x++)
+    console.log(getComputerChoice(words));
